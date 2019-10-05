438a9ae5f1e28d3cc64df880379ce41fe4d6ba9e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:43:37 +0200

    Conversioni in base: 2, 8, 16

--- DecToBin/Program.cs
+++ DecToBin/Program.cs
@@ -8,8 +8,34 @@ namespace DecToBin
         {
             Console.WriteLine("Scrivi un numero");
             int n=int.Parse(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(n, 2));
+
+            Console.WriteLine("Seleziona in che base lo vuoi convertire: 2,8,16");
+            string selezione = (Console.ReadLine());
+            try
+            {
+                if (selezione == "2")
+                {
+                    Console.WriteLine(Convert.ToString(n, 2));
+
+                }
+                else if (selezione == "8")
+                {
+                    Console.WriteLine(Convert.ToString(n, 8));
+
+                }
+                else if (selezione == "16")
+                {
+
+                    Console.WriteLine(Convert.ToString(n, 16));
+                }
+
+            }
+            catch (Exception e) {
+
+            }
+        
             Console.ReadLine();
+  
 
         }
     }
